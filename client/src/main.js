9623ea21d5cb33b7403fd91cdbf518145ea72396
Hlib612 <glebmalonos@gmail.com>
2025-12-04 19:41:36 +0200

    add s

--- client/src/main.js
+++ client/src/main.js
@@ -1,3 +1,74 @@
 import './style.css'
 
 fetch(`http://localhost:3000/students`)
+
+// Функція для отримання всіх студентів
+
+const options1 ={
+    method: "GET",
+}
+
+
+
+
+const getStudents = () => {
+
+ fetch(`http://localhost:3000/students` , options1)
+ .then(result =>{
+    console.log(result)
+    result.json()
+ })
+
+
+ }
+
+ getStudents()
+
+
+
+// Функція для відображення студентів у таблиці
+
+function renderStudents(students) {
+
+ // твій код
+
+ 
+
+}
+
+
+
+// Функція для додавання нового студента
+
+function addStudent(e) {
+
+ // твій код
+
+  
+
+}
+
+
+
+// Функція для оновлення студента
+
+function updateStudent(id) {
+
+ // твій код
+
+
+
+ }
+
+
+
+// Функція для видалення студента
+
+function deleteStudent(id) {
+
+    // твій код
+
+}
+
+
+
